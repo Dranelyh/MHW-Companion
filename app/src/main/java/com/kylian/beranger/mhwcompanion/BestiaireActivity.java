ea0c87aab54f82427ba34763943fbf63182e6f94
Kylian <kylianbdu13@gmail.com>
2024-06-28 13:50:43 +0200

    Bestiaire

--- app/src/main/java/com/kylian/beranger/mhwcompanion/BestiaireActivity.java
+++ app/src/main/java/com/kylian/beranger/mhwcompanion/BestiaireActivity.java
@@ -1,6 +1,9 @@
 package com.kylian.beranger.mhwcompanion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,14 @@ public class BestiaireActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bestiaire);
+    }
+
+    public void lien(View view) {
+        String monster = view.getTag().toString();
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://monsterhunterworld.wiki.fextralife.com/" + monster));
+        startActivity(intent);
     }
 }
