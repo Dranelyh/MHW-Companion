2f9b224af9e1011221ec7ec086ca2754cc759deb
Kylian <kylianbdu13@gmail.com>
2024-06-29 15:34:56 +0200

    Ajout Carte

--- app/src/main/java/com/kylian/beranger/mhwcompanion/CarteActivity.java
+++ app/src/main/java/com/kylian/beranger/mhwcompanion/CarteActivity.java
@@ -1,6 +1,8 @@
 package com.kylian.beranger.mhwcompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,34 @@ public class CarteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_carte);
+    }
+    public void asteraClick(View view) {
+        Intent intentAstera = new Intent(this , AsteraActivity.class);
+        startActivity(intentAstera);
+    }
+
+    public void foretClick(View view) {
+        Intent intentForet = new Intent(this , ForetActivity.class);
+        startActivity(intentForet);
+    }
+
+    public void desertClick(View view) {
+        Intent intentDesert = new Intent(this , DesertActivity.class);
+        startActivity(intentDesert);
+    }
+
+    public void corailClick(View view) {
+        Intent intentCorail = new Intent(this , CorailActivity.class);
+        startActivity(intentCorail);
+    }
+
+    public void valClick(View view) {
+        Intent intentVal = new Intent(this , ValActivity.class);
+        startActivity(intentVal);
+    }
+
+    public void elderClick(View view) {
+        Intent intentElder = new Intent(this , ElderActivity.class);
+        startActivity(intentElder);
     }
 }
